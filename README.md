8429a2b2ec0f6f5c5f0ec44bf8f4dca983d50de0
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-05-21 18:41:26 +0300

    Application updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # spring-boot-keycloak
+
+#Get auth key
+`curl -ss --data "grant_type=password&client_id=curl&username=joe_admin&password=admin" http://localhost:8080/auth/realms/spring-security-example/protocol/openid-connect/token`
+
+#use application
+`curl -H "Authorization: bearer [your-token]" http://localhost:9002/admin/hello`
+
+https://sandor-nemeth.github.io/java/spring/2017/06/15/spring-boot-with-keycloak.html
---
+++ src/main/java/com/mozdzo/keycloak/HelloEndpoint.java
@@ -0,0 +1,20 @@
+package com.mozdzo.keycloak;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloEndpoint {
+    @GetMapping("/admin/hello")
+    @Secured("ROLE_ADMIN")
+    public String sayHelloToAdmin() {
+        return "Hello Admin";
+    }
+
+    @GetMapping("/user/hello")
+    @Secured("ROLE_USER")
+    public String sayHelloToUser() {
+        return "Hello User";
+    }
+}
