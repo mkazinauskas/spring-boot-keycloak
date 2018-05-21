d8d3aef1e4d0dc972b6b13250043ff5d26044cb7
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-05-21 17:41:17 +0300

    Initial

---
+++ src/main/groovy/com/mozdzo/keycloak/KeycloakApplication.groovy
@@ -0,0 +1,12 @@
+package com.mozdzo.keycloak
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KeycloakApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run KeycloakApplication, args
+	}
+}
