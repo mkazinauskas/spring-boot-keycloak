2ccaeb2e40686646208b1b86b58764b208747afb
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-08-01 23:44:25 +0300

    Test updates

--- README.md
+++ README.md
@@ -28,3 +28,7 @@
 # Update realms and users
 ## Edit `docker/keycloak/setup/setup.sh`
 ## Run this file inside your local keycloak installation bin folder
+
+
+Testing
+https://github.com/keycloak/keycloak-quickstarts/blob/latest/app-authz-rest-springboot/pom.xml
--- src/test/groovy/com/mozdzo/keycloak/KeycloakApplicationTests.groovy
+++ src/test/groovy/com/mozdzo/keycloak/KeycloakApplicationTests.groovy
@@ -1,13 +1,23 @@
 package com.mozdzo.keycloak
 
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.keycloak.test.TestsHelper
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
 class KeycloakApplicationTests {
+
+	@BeforeClass
+	public static void setup() throws IOException {
+		TestsHelper.baseUrl = "http://localhost:8080";
+		TestsHelper.testRealm="spring-boot-quickstart";
+		TestsHelper.importTestRealm("admin","admin","/quickstart-realm.json");
+		TestsHelper.createDirectGrantClient();
+	}
 
 	@Test
 	void contextLoads() {
