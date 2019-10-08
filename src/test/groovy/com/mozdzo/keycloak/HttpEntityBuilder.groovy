59271f39baeb0373fe70223dab827eb416a7a95b
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2019-10-08 22:18:59 +0300

    Unused method removal

--- src/test/groovy/com/mozdzo/keycloak/HttpEntityBuilder.groovy
+++ src/test/groovy/com/mozdzo/keycloak/HttpEntityBuilder.groovy
@@ -4,8 +4,6 @@ import groovy.transform.CompileStatic
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 
-import java.nio.charset.Charset
-
 @CompileStatic
 class HttpEntityBuilder<B> {
 
@@ -24,15 +22,6 @@ class HttpEntityBuilder<B> {
 
     HttpEntityBuilder<B> bearer(String token) {
         headers.add('Authorization', "Bearer ${token}".toString())
-        return this
-    }
-
-    HttpEntityBuilder<B> basic(String username, String password) {
-        String auth = "${username}:${password}"
-
-        String encoded = Base64.encoder.encodeToString(auth.getBytes(Charset.forName('US-ASCII')))
-
-        headers.add('Authorization', "Basic ${encoded}")
         return this
     }
 
